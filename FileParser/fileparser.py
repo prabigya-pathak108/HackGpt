29202f986828e749b561a0f061aae04182179bcd
Prabigya Pathak <120402472+prabigya-pathak108@users.noreply.github.com>
2024-11-21 22:14:55 +0545

    CSV Parser Cleaned

--- FileParser/fileparser.py
+++ FileParser/fileparser.py
@@ -14,17 +14,8 @@ class CSVParser(FileParserBaseClass):
         try:
             with open(self.file_name) as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=',')
-                line_count = 0
-                for row in csv_reader:
-                    if line_count == 0:
-                        print(f'Column names are {", ".join(row)}')
-                        line_count += 1
-                    else:
-                        print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                        line_count += 1
-                print(f'Processed {line_count} lines.')
-                return "CSV files will be later processed..."
-        except Exception as e:
+                return csv_reader
+        except:
             return None
 
 
